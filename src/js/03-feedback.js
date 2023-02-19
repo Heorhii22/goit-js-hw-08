470716ed1338095c83439a95a6c8e75a44d4ed96
George Artemov <oscurasgeorge@gmail.com>
2023-02-19 01:13:07 +0000

    bug-fix-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,41 @@
 import throttle from "lodash.throttle";
 
 const STORAGE_KEY = "feedback-form-state";
-const formData = {};
-const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let formData = {};
+const data = localStorage.getItem(STORAGE_KEY);
+const parsedData = JSON.parse(data);
 
 const formEl = document.querySelector(".feedback-form");
 const inputEl = document.querySelector("input");
 const textareaEl = document.querySelector("textarea");
 
-populateTextArea();
 
 formEl.addEventListener("input", throttle(onFormInputType, 500));
 formEl.addEventListener("submit", onFormSubmit);
 
+populateTextArea();
+
 function onFormInputType(e) {
-  formData[e.target.name] = e.target.value;
+  formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log(formData);
+  
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+  
+  console.log(savedData);
+  
   e.currentTarget.reset();
+  
   localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
 
 function populateTextArea() {
-  const data = localStorage.getItem(STORAGE_KEY);
   if (data) {
-    inputEl.value = parsedData.email;
-    textareaEl.value = parsedData.message;
+    inputEl.value = parsedData.email || '';
+    textareaEl.value = parsedData.message || '';
   }
 }
